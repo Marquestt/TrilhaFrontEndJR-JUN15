399e288b3cf04e798be8eaea47cfdb66cfc99f0f
Marquestt <thatyanecmarques98@live.com>
2024-08-14 17:53:47 -0300

    imagem da desenvolvedora adicionada ao projeto

--- projeto-codigo-certo/src/pages/Home/index.js
+++ projeto-codigo-certo/src/pages/Home/index.js
@@ -1,9 +1,11 @@
 import styles from './Home.module.css'
+import imagemProfile from '../Home/imagens/profile.jpg'
 
 const Home = () => {
     return(
         <main className={styles.mainContainer}>
             <h2>Sobre mim:</h2>
+            <img src={imagemProfile} alt='Imagem de Thatyane Marques sorrindo'/>
         </main>
     )
 }
